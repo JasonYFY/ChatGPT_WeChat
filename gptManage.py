4d4df1096762898d4be7843d03c1f98e3674c8a7
JasonYFY <996335280@qq.com>
2023-05-29 10:15:46 +0800

    Update gptManage.py

--- gptManage.py
+++ gptManage.py
@@ -459,8 +459,8 @@ class gptMessageManage(object):
                     chunk_time = time.time() - start_time
                     if chunk_time>=timeout:
                         print('请求中断')
-                        full_reply_content = ''.join(collected_messages).join('\n 还有剩余结果，请回复【继续】查看！')
-                        return {'code':0,'content':full_reply_content}
+                        full_reply_content = ''.join(collected_messages)
+                        return {'code':0,'content':full_reply_content+'\n 还有剩余结果，请回复【继续】查看！'}
                         break
                     chunk = response.__next__()
                     collected_chunks.append(chunk)  # save the event response
