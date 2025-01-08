8b496b6ec0af490f37db78bc1c1582e6ab14b21c
yifangyu <31659735+yifangyujason@users.noreply.github.com>
2025-01-08 10:15:29 +0800

    feat:删除无用代码

--- testBard.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-import os
-
-genai.configure(api_key="AIzaSyCbv-uIRcEFL4YTGxk1yoXHGiRDE0KCHwA")
-model = genai.GenerativeModel('gemini-1.0-pro-latest')
-response = model.generate_content("The opposite of hot is")
-print(response.text)
